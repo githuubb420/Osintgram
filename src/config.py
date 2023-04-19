eeb49b8997789cc82a55dda0ec1a878b772b7d3a
Sourcery AI <>
2023-04-19 19:42:02 +0000

    'Refactored by Sourcery'

--- src/config.py
+++ src/config.py
@@ -10,7 +10,7 @@ except FileNotFoundError:
     pc.printout('Error: file "config/credentials.ini" not found!\n', pc.RED)
     sys.exit(0)
 except Exception as e:
-    pc.printout("Error: {}\n".format(e), pc.RED)
+    pc.printout(f"Error: {e}\n", pc.RED)
     sys.exit(0)
 
 def getUsername():
